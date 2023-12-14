e150f26b725fd0a06494c1f7d15fc0800300bf49
Thiago Oliveira <thiago.i.oliveira@aluno.senai.br>
2023-12-14 18:34:48 -0300

    cards e css

--- games-api/js/main.js
+++ games-api/js/main.js
@@ -1,56 +1,62 @@
 import { getAllGames, createGame, deleteGame, updateGame } from "./service.js";
 
 window.onload = () => {
-    loadGames();
+  loadGames();
 };
-//refatorado
+
 const loadGames = () => {
-    console.log('>>>')
-    const dataContainer = 
-        document.getElementById('data-container');
-        getAllGames().then(resp =>{
-        resp.forEach(game => {
-            const gamesElement = 
-                document.createElement('div');
-                gamesElement.innerHTML = 
-`<strong>${game.nome}</strong><p>${game.preco}</p>`;
-             dataContainer.appendChild(gamesElement);
-        });
-    })
+  const dataContainer = document.getElementById("data-container");
+  getAllGames().then((resp) => {
+    resp.forEach((jogo) => {
+      const gamesElement = document.createElement("div");
+      gamesElement.innerHTML = `
+     
+      <div class="elemento">
+      <img class="cardimg" src="${jogo.img}" alt="${jogo.nome}">
+    
+      <hr>
+     
+    <h5 class="cardtitle">${jogo.nome}</h5>
+    
+    <hr>
+  
+    <p class="cardtext"> R$ ${jogo.preco}</p>       
+      </div>
+      `;
+
+      dataContainer.appendChild(gamesElement);
+    });
+  });
 };
 
-// Refatorado
-document.getElementById('btnCreate') 
-.addEventListener('click', () => {
-    const jogo = {
-        "nome": "The legend of Zelda",
-        "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
-        "preco": 100
-    };
-    createGame(jogo);
-}),
+document.getElementById("btnCreate").addEventListener("click", () => {
+  const jogo = {
+    nome: "The legend of Zelda",
+    img: "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+    preco: 100,
+  };
 
-// Refatorado
-document.getElementById('btnDelete')
-.addEventListener('click', () =>{
-    const jogo = {
-        "nome": "The legend of Zelda",
-        "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
-        "preco": 300,
-        "id": 3
-    };
-    deleteGame(jogo);
+  createGame(jogo);
 });
 
-// Refatorado
-document.getElementById('btnUpdate')
-.addEventListener('click', () => {
-    const jogo = {
-        "nome": "The Legend of Zelda 2",
-        "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
-        "preco": 300,
-        "id": 3
-    };
+document.getElementById("btnDelete").addEventListener("click", () => {
+  const jogo = {
+    nome: "The legend of Zelda 2",
+    img: "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+    preco: 300,
+    id: 3,
+  };
 
-    updateGame(jogo)
+  deleteGame(jogo);
+});
+
+document.getElementById("btnUpdate").addEventListener("click", () => {
+  const jogo = {
+    nome: "The legend of Zelda 2",
+    img: "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+    preco: 300,
+    id: 3,
+  };
+
+  updateGame(jogo);
 });
